030d96fa44c1f720413ce3b07cbf6d466241b500
DoniG09 <81458410+DoniG09@users.noreply.github.com>
2021-09-15 01:30:58 -0600

    Modificaciones en el proyecto cpp en la rama master como prueba por parte de Doni

--- DreamTeam.cpp
+++ DreamTeam.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main(){
     
+    cout<<endl; //Salto de línea
+    
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),3); /*Asignación del color azul al mensaje que aparecerá 
+	en pantalla*/
+    cout<<"Doni está escribiendo esta línea como prueba"<<endl;
+	cout<<endl; //Salto de línea
+	
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),11); /*Asignación del color cian al mensaje que aparecerá 
+	en pantalla*/
+    cout<<"Algo me dice que ya chingué el return :'v"<<endl;
+    
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),7); /*Asignación del color blanco*/
+    
     return 0;
+    
+    
+    
 }
